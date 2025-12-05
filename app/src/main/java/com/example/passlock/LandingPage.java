51fe834c0bb84c7795a6bcc08c1cf9dd6959e95a
TheJoe04 <josmolina@csumb.edu>
2025-12-04 17:31:40 -0800

    Fixed Admin LogIn not working... and changed UI for MainActivity. Hacker Theme

--- app/src/main/java/com/example/passlock/LandingPage.java
+++ app/src/main/java/com/example/passlock/LandingPage.java
@@ -16,13 +16,13 @@ public class LandingPage extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences(MainActivity.PREFS_NAME, MODE_PRIVATE);
         boolean isAdmin = prefs.getBoolean("isAdmin", false);
         String username = prefs.getString("username", "User");
-
+//        Checks and loads appropriate layout/landing page.
         if (isAdmin) {
             setContentView(R.layout.landing_page_admin);
         } else {
             setContentView(R.layout.landing_page_user);
         }
-
+//        Shared UI (both pages have this)
         TextView welcomeTextView = findViewById(R.id.textView);
         welcomeTextView.setText("Welcome " + username);
 
@@ -42,6 +42,34 @@ public class LandingPage extends AppCompatActivity {
             BtnNewPassLock.setOnClickListener(v -> {
                 startActivity(new Intent(this, NewPassLockActivity.class));
             });
+        }
+//        buttons for admin
+        Button viewUserPasslocksBtn = findViewById(R.id.viewUserPasslocksBtn);
+        if (viewUserPasslocksBtn != null) {
+            viewUserPasslocksBtn.setOnClickListener(v ->
+                    startActivity(new Intent(this, ViewUserPasslocksActivity.class))
+            );
+        }
+
+        Button editSuggestedPasslocksBtn = findViewById(R.id.editSuggestedPasslocksBtn);
+        if (editSuggestedPasslocksBtn != null) {
+            editSuggestedPasslocksBtn.setOnClickListener(v ->
+                    startActivity(new Intent(this, EditSuggestedPasslocksActivity.class))
+            );
+        }
+
+        Button compareUserPasslocksBtn = findViewById(R.id.compareUserPasslocksBtn);
+        if (compareUserPasslocksBtn != null) {
+            compareUserPasslocksBtn.setOnClickListener(v ->
+                    startActivity(new Intent(this, ComparePasslocksActivity.class))
+            );
+        }
+
+        Button managePasswordTipsBtn = findViewById(R.id.managePasswordTipsBtn);
+        if (managePasswordTipsBtn != null) {
+            managePasswordTipsBtn.setOnClickListener(v ->
+                    startActivity(new Intent(this, ManagePasswordTipsActivity.class))
+            );
         }
     }
 }
